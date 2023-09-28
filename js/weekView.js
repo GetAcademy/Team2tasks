274e5842b017d3d5562e1e5d6ac9b858c542809a
GET Academy <terje@kolderup.net>
2023-09-28 13:25:31 +0200

    Initial commit

---
+++ js/weekView.js
@@ -0,0 +1,22 @@
+function updateViewWeek(){
+    document.getElementById('app').innerHTML = /*HTML*/`
+        ${createMenuHtml()}
+        <h1>Uke</h1>
+        <ul>
+            ${createTaskHtml()}
+        </ul>
+    `;
+}
+
+function createTaskHtml(){
+    let html = '';
+    for(let task of model.tasks){
+        html += /*HTML*/`
+            <li>            
+                ${task.title} - 
+                ${task.description}
+            </li>
+        `;
+    }
+    return html;
+}
